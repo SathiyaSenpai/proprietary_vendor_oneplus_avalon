5c470e10df9473c8c5631bd244e92ed64719215f
SathiyaSenpai <25bai042@hicas.ac.in>
2026-08-14 02:42:41 +0530

    sm8650-common: configs: Reset perf boost configs to qcom

---
+++ Android.mk
@@ -0,0 +1,35 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),avalon)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,31ffa12dae40bcf75b551bdee6ad17c321efbb03)
+$(call add-radio-file-sha1-checked,radio/aop.img,5da26126914721e057886fac9d6cef92270dde68)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,d49e76c3a9f1f402d8bf4dbbc18750f37a547d29)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,ad90506b589ed538f8bbfbaaa8f5806e1a87960b)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,67261a3d7435f34cfaecf4877989202804b937ee)
+$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,fe52c989d863c46c30d18071c881c5bf12241702)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,2cd0e54da2c06f1743801a53dd6049cc780551b1)
+$(call add-radio-file-sha1-checked,radio/dsp.img,7e81146e07bb4120616a084cc932dc4836420d29)
+$(call add-radio-file-sha1-checked,radio/engineering_cdt.img,30b51d9bd681f739f88175d738756101af8cbbdb)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,007fa454d5c62dab041cc96ecd99eab474a4eb9c)
+$(call add-radio-file-sha1-checked,radio/hyp.img,b168c044a819acc37df127a591a00ba018febd3d)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,fa14740c79c2f5c99f2549190d5165d9582a81fe)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,b432062db66083b63377c71f188825e4b2edab71)
+$(call add-radio-file-sha1-checked,radio/modem.img,3b8c28eefe7543c9546b02359a26918374b41ae8)
+$(call add-radio-file-sha1-checked,radio/oplus_sec.img,fb7d5b89feac2ef035e98e945d5f094e8d1ed95c)
+$(call add-radio-file-sha1-checked,radio/oplusstanvbk.img,f6075a8226267c24e1d56b0911da058b51df1a9e)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,28bb069571e0dcc7aad1c5827af3964460197c66)
+$(call add-radio-file-sha1-checked,radio/shrm.img,425f7f96f453b08bf07c66ca051352e794491454)
+$(call add-radio-file-sha1-checked,radio/splash.img,fc5da5e9d4d388820d09510b3ed2ecb895fef5fe)
+$(call add-radio-file-sha1-checked,radio/tz.img,acb9c68324077118d5e349c5c0db1003774c093e)
+$(call add-radio-file-sha1-checked,radio/uefi.img,ca1e123477766753bce125d02af6fbfc7b7978ef)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,4da4cd270fc4e802447c46006495fa739481adfa)
+$(call add-radio-file-sha1-checked,radio/xbl.img,6edf252acb52c0a3fca30968a66929abba3e668f)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,af534c3f43c8cbd65888cc54017a6b34c4926247)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,80cbd1dc6057d8e666329a610085cb3bf6c6eb85)
+
+endif
